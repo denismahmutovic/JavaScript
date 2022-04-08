08520d906871f566fd621d8cd697768c9824804a
Denismahmutovic <djdeny95@gmail.com>
2022-04-08 16:13:39 +0200

    home

--- Pocetak/app.js
+++ Pocetak/app.js
@@ -1,2 +1,11 @@
 const colors = ["green", "red", "rgba(133, 122, 2900)", "#f15025"];
-const btn = document.getElementById();
+const btn = document.getElementById("btn");
+const color = document.querySelector (".color")
+btn.addEventListener("click", function () {
+    const randomnumber = getRandomNumber();
+    document.body.style.backgroundColor = colors [randomnumber];
+    color.textContent = color[randomnumber]
+})
+function getRandomNumber (){
+    return Math.floor(Math.random() * colors.length );    
+}
