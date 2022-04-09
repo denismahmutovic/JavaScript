113561495cfb9752e13dd2bf4a6850c769475525
Denismahmutovic <djdeny95@gmail.com>
2022-04-09 22:35:34 +0200

    vezba

--- Pocetak/app.js
+++ Pocetak/app.js
@@ -1,11 +1,34 @@
-const colors = ["green", "red", "rgba(133, 122, 2900)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector (".color")
-btn.addEventListener("click", function () {
-    const randomnumber = getRandomNumber();
-    document.body.style.backgroundColor = colors [randomnumber];
-    color.textContent = color[randomnumber]
-})
-function getRandomNumber (){
-    return Math.floor(Math.random() * colors.length );    
-}
+// const colors = ["green", "red", "rgba(133, 122, 2900)", "#f15025"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector (".color")
+// btn.addEventListener("click", function () {
+//     const randomnumber = getRandomNumber();
+//     document.body.style.backgroundColor = colors [randomnumber];
+//     color.textContent = color[randomnumber]
+// })
+// function getRandomNumber (){
+//     return Math.floor(Math.random() * colors.length );    
+// }
+// console.log("Hello world", "prba");
+// console.log ("dasdsa");
+// let imePrezime = "DJ denNY";
+// console.log("poruka");
+// let kovanice = 10;
+// console.log("broj kovanica:", kovanice);
+// kovanice = 15;
+// console.log
+
+// let posiljatelj  = prompt("tko salje dumpcoin?");
+// let primatelj = prompt("kome saljes dumpcoin");
+// conlose.log(posiljatelj, "salje dc", primatelj);
+
+// document.getElementById("demo").style.fontSize = "35px";
+document.getElementById("demo").innerHTML = 5 * 6;
+// document.getElementById("demo").innerHTML = 2+2;
+document.getElementById("demo").innerHTML = 1-1;
+document.write (10+10);
+window.alert(5 + 6);
+alert(5 + 6);
+
+
+
