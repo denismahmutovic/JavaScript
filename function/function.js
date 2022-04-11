628d4c598123b2801a324a8647d9aafaed6484a5
Denismahmutovic <djdeny95@gmail.com>
2022-04-11 16:10:35 +0200

    function

---
+++ function/function.js
@@ -0,0 +1,25 @@
+// function btn (){
+//     alert("pozdrav")
+// }
+// function fun (x) {
+//    alert ("hello " + x);
+
+// }
+// fun ("denis");
+
+
+
+// function denis (x){
+//     alert ("pozdrav " + x);
+// }
+// denis("denise");
+// denis ("amare");
+// denis ("emire");
+
+//  function sa vise parametra:
+
+function fun (jedan, dva, tri){
+    document.write (jedan + " je bolji od " + dva + tri );
+    
+}
+fun ("Fifa", "pesa", " i od f1")
