22603c3bca59dea06c73eb4aa0fd880e43a30c3f
Denismahmutovic <djdeny95@gmail.com>
2022-04-11 17:38:21 +0200

    function

--- function/function.js
+++ function/function.js
@@ -18,8 +18,66 @@
 
 //  function sa vise parametra:
 
-function fun (jedan, dva, tri){
-    document.write (jedan + " je bolja od " + dva + tri );
+// function fun (jedan, dva, tri){
+//     document.write (jedan + " je bolja od " + dva + tri );
     
-}
-fun ("Fifa", "pesa", " i od f1")
+// }
+// fun ("Fifa", "pesa", " i od f1")
+
+                    // [povratne infromacije]
+
+// function broj (a, b){
+//     var c = a+b;
+//     return c;
+// }
+// document.write(broj (100, 100) );
+
+                    //  [pozivanje vise fukcija]
+
+// function jedan (){
+//     document.write("demo")
+// }
+// function dva (){
+//     document.write (" proba dema");
+// }
+// function start (){
+//     jedan();
+//     dva();
+// }
+// start();
+// function proba (){
+//     document.write("create");
+
+// }
+// function probaa(){
+//     document.write(" demo  folder");
+
+// }
+// function start(){
+//     proba()
+//     probaa()
+// }
+// start()
+
+                    // [Globalne i lokalne variable]
+        // globalna            
+//   var ime = "denis";
+//   function fun (){
+//       document.write (ime);
+//   }
+//   fun();
+//   document.write(ime);
+
+        // lokalna
+  function fun (){
+    var ime = "denis";
+      document.write (ime);
+  }
+  fun();
+  document.write(ime);
+
+
+
+
+
+
