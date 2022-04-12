2733785b6d75e8eb82673ae2ce30c49bdfafca5d
Denismahmutovic <djdeny95@gmail.com>
2022-04-12 15:33:58 +0200

    vezba

---
+++ Operators/Operators.js
@@ -0,0 +1,24 @@
+// DECLARATION
+var x;
+var y;
+var x;
+var a, b, c, d, e;
+
+// INSTANTIATION
+x = 1; // number
+y = 2;
+z = "neki broj"; //string
+a = true; // boolean
+var niz = [1, 2, 3, 4,5]; // array (niz)
+
+// CONCATENATIOON
+// document.getElementById("js").innerHTML =  niz;
+console.log (typeof niz);
+
+for (i = 0; i < niz.length; i++) {
+    document.write(niz [i]);
+
+}
+
+// console.log (x==z);
+// console.log (x===z);
