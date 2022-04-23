d50f35bb3b9ac89501ede2cfe9594c33897a48b3
Denismahmutovic <djdeny95@gmail.com>
2022-04-24 00:24:10 +0200

    vezba2

--- vezba-dva/vezba.js
+++ vezba-dva/vezba.js
@@ -2,37 +2,26 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn")
 
-btns.forEach(function(btn){
-    btn.addEventListener("click", function(e) {
+btns.forEach (function(btn){
+    btn.addEventListener("click", function(e){
         const styles = e.currentTarget.classList;
-        if (styles.contains("minus")){
+        if(styles.contains ("minus")){
             count --;
         } else if(styles.contains ("plus")){
             count ++;
         } else{
             count = 0;
         }
-        if(count > 0){
-            value.style.color = "green";
+        if (count > 0){
+            value.style.color = "red"
+
         }
         if(count < 0){
             value.style.color = "red";
         }
         if(count === 0){
-            value.style.color = "blue";
+            value.style.color = "red"
         }
         value.textContent = count;
-    });
-    console.log(value);
-});
-
-
-
-
-//     btns.forEach(function(btn){
-//         btn.addEventListener("click",function(e){
-//             const styles = e.currentTarget.classlist;
-//         })
-//     })
-
-
+    })
+})
