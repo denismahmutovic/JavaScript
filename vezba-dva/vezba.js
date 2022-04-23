fdff475e9c9c18ab87bebcc6f3457defe032d96b
Denismahmutovic <djdeny95@gmail.com>
2022-04-23 22:59:15 +0200

    vezba2

--- vezba-dva/vezba.js
+++ vezba-dva/vezba.js
@@ -1,47 +1,38 @@
-
-let count = 0 // pocetna vrednost 0
-
+let count = 0;
 const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll(".btn")
 
 btns.forEach(function(btn){
-    btn.addEventListener ("click", function(e) {
-
-    const styles = e.currentTarget.classList;
-    
-    if (styles.contains ("decrease")){
-        count --;
-
-    } else if (styles.contains("inrease")) {
-        count ++;
-    } else{ 
-        count = 0;
-    }
-        if (count > 0){
-            value.style.color = "#f9346c";
-        } 
-        if (count < 0) {
-            value.style.color = "red";
-        } 
-        if (count === 0){
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("minus")){
+            count --;
+        } else if(styles.contains ("plus")){
+            count ++;
+        } else{
+            count = 0;
+        }
+        if(count > 0){
             value.style.color = "green";
         }
-
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count === 0){
+            value.style.color = "blue";
+        }
         value.textContent = count;
-    
     });
+    console.log(value);
 });
 
 
-let vrednost = 0;
-let value = document.querySelector("#value");
-let btns = document.querySelectorAll(".btn");
 
-btns.forEach(function(btn) {
-    btn,addEventListener("click",function(e){
-        const styles = e.currentTarget.classList;
-        if()
-    }
-    )
-}
-)
+
+//     btns.forEach(function(btn){
+//         btn.addEventListener("click",function(e){
+//             const styles = e.currentTarget.classlist;
+//         })
+//     })
+
+
