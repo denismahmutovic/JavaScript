f9591f8f87e42f85693a07bde25c7ea64614259a
Denismahmutovic <djdeny95@gmail.com>
2022-04-25 16:03:16 +0200

    Slideshow

--- vezba-dva/vezba.js
+++ vezba-dva/vezba.js
@@ -2,26 +2,51 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn")
 
-btns.forEach (function(btn){
-    btn.addEventListener("click", function(e){
-        const styles = e.currentTarget.classList;
-        if(styles.contains ("minus")){
-            count --;
-        } else if(styles.contains ("plus")){
-            count ++;
-        } else{
-            count = 0;
-        }
-        if (count > 0){
-            value.style.color = "red"
+// btns.forEach (function(btn){
+//     btn.addEventListener("click", function(e){
+//         const styles = e.currentTarget.classList;
+//         if(styles.contains ("minus")){
+//             count --;
+//         } else if(styles.contains ("plus")){
+//             count ++;
+//         } else{
+//             count = 0;
+//         }
+//         if (count > 0){
+//             value.style.color = "red"
 
-        }
-        if(count < 0){
-            value.style.color = "red";
-        }
-        if(count === 0){
-            value.style.color = "red"
-        }
-        value.textContent = count;
-    })
+//         }
+//         if(count < 0){
+//             value.style.color = "red";
+//         }
+//         if(count === 0){
+//             value.style.color = "red"
+//         }
+//         value.textContent = count;
+//     })
+// })
+
+
+btns.forEach (function(btn){
+   btn.addEventListener("click", function(e){
+       const styles = e.currentTarget.classList;
+       if(styles.contains ("minus")){
+         count --;
+       } else if (styles.contains("plus")){
+           count ++;
+       } else{
+           count = 0;
+       }
+       if(count > 0){
+           value.style.color = "red";
+       }
+       if(count < 0){
+           value.style.color = "red";
+       }
+       if(count === 0){
+           value.style.color = "red"
+       }
+       value.textContent = count;
+
+   })
 })
