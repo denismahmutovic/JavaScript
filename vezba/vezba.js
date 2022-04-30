6b73f06ed5cea418d207c891691ea52ec96d66a8
Denismahmutovic <djdeny95@gmail.com>
2022-04-30 23:19:15 +0200

    vezba

--- vezba/vezba.js
+++ vezba/vezba.js
@@ -336,20 +336,30 @@
 
 
 
-function vezba(){
-    let e =document.querySelector('#pass');
-    e.addEventListener('keyup', addName)
-}
+// function vezba(){
+//     let e =document.querySelector('#pass');
+//     e.addEventListener('keyup', addName)
+// }
 
-function addName(){
-    let e = document.querySelector('#pass');
-    let sifra = e.value;
-    let message = '';
+// function addName(){
+//     let e = document.querySelector('#pass');
+//     let sifra = e.value;
+//     let message = '';
 
-    if(sifra.length < 5 || sifra.length >10){
-        message ='passwor is univailable';
-    }
-        let mes = document.querySelector('#message');
-        mes.innerHTML=message;
-}   
-window.addEventListener('load', vezba);
+//     if(sifra.length < 5 || sifra.length >10){
+//         message ='passwor is univailable';
+//     }
+//         let mes = document.querySelector('#message');
+//         mes.innerHTML=message;
+// }   
+// window.addEventListener('load', vezba);
+
+
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+numbers.forEach(myFunction);
+document.getElementById("demo").innerHTML = txt;
+
+function myFunction(value, index, array) {
+  txt += value + "<br>"; }
