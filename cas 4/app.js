a1c93f67739321e70461c4b2dc685b48ab7208c8
Denismahmutovic <djdeny95@gmail.com>
2022-05-14 18:39:36 +0200

    cas 4

--- cas 4/app.js
+++ cas 4/app.js
@@ -14,20 +14,83 @@
 
 // U datom stringu zadnje slovo svake reci prebaciti da bude veliko
 
-let  string = "ovo je string ";
-let newString = "";
-let len = string.length;
+// let  string = "ovo je string ";
+// let newString = "";
+// let len = string.length;
 
 
-for (let i = 0; i < len; i++) {
-    if (i === len -1) {
-        newString += string[i].toUpperCase();
-    } else if (string[i + 1] === " ") {
-        newString += string[i].toUpperCase();
-    } else {
-        newString += string[i];
+// for (let i = 0; i < len; i++) {
+//     if (i === len -1) {
+//         newString += string[i].toUpperCase();
+//     } else if (string[i + 1] === " ") {
+//         newString += string[i].toUpperCase();
+//     } else {
+//         newString += string[i];
+//     }
+// }
+
+// console.log(newString);
+
+
+
+
+
+
+
+
+let recenica = "danas je subota"
+let prazna = ""
+let brojac = recenica.length
+
+for (let i = 0; i < brojac; i ++){
+
+    if(i === brojac -1){
+        prazna+= recenica[i].toUpperCase();
+    } else if(recenica[i+1] === " " ){
+        prazna+= recenica[i].toUpperCase();
+    } else{
+        prazna += recenica [i]
     }
 }
+console.log(prazna)
 
-console.log(newString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
