e4b5a5661b8fc8837e737b2fffd8b6f0f3a8819f
Denismahmutovic <djdeny95@gmail.com>
2022-05-19 15:43:44 +0200

    cas6

--- cas 6/app.js
+++ cas 6/app.js
@@ -8,18 +8,71 @@
 // cars.length = 0  // prazan niz
 
         //index
- const cars = ["BMW", 4, true, null, undefined, "vW"];
+//  const cars = ["BMW", 4, true, null, undefined, "vW"];
 //  for(let i = 0; i < cars.length; i++){
 //      if(cars[i]){
 //          console.log(cars[i])
 //      }
 //     }  
 
-    for(let car in cars){  // in se koristi samo za nizove
-        console.log(car)
-    };
+    // for(let car in cars){  // in se koristi samo za nizove
+    //     console.log(car)
+    // };
 
-    const studenti = ["marko", "janko", 'sasa', "zika"]
+    // const studenti = ["marko", "janko", 'sasa', "zika"]
 
-    studenti [8] = "zika"
-    console.log(studenti)
+    // studenti [8] = "zika"
+    // console.log(studenti)
+// ---------------------------- HOME--------------------------------------------------
+
+const play_lista = ["Eliti-Odredi","Rasta","Nucci", "Voyage", "Coby", null, false]
+ 
+let duzina = play_lista.length
+
+for(let i = 0; i < duzina; i++){
+    if(play_lista[i]){
+        console.log(play_lista[i])
+    }
+}
+const play = ["Eliti-Odredi","Rasta","Nucci", "Voyage", "Coby","Denis","Marko"]
+console.log(play.length)
+play[0]= "MARe"
+console.log(play[6])
+play.length = 0
+console.log(play)
+
+const studenti  = ["Amina", "sara","Hana"]
+for (let student of studenti){
+    console.log(student)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
