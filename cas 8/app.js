741d1e09ccd0689341af887d5f4fab4f71e9750c
Denismahmutovic <djdeny95@gmail.com>
2022-05-26 13:09:59 +0200

    cas8

--- cas 8/app.js
+++ cas 8/app.js
@@ -35,35 +35,64 @@
 
 
 // dodavanje na pocetnu niza unshift
-const cars = ["bmw", "audi","smart", "mercefed", "mini","fiat"]
-cars.unshift ("ford")
-cars.unshift ("katilak")
-//brisanje prvog clana u nizu
-cars.shift ()
-const shiftValue =cars.shift() //ubaciabnje niza u conconel.log
+// const cars = ["bmw", "audi","smart", "mercefed", "mini","fiat"]
+// cars.unshift ("ford")
+// cars.unshift ("katilak")
+// //brisanje prvog clana u nizu
+// cars.shift ()
+// const shiftValue =cars.shift() //ubaciabnje niza u conconel.log
 
-//obrniti niz
-cars.reverse() // okrecanje niza zadnji da bude prvi
+// //obrniti niz
+// cars.reverse() // okrecanje niza zadnji da bude prvi
 
-//spajanje dva niza  -contact (niz)
-const imena = ["faris","isko", "aldin"]
-const age = [20,100,50,120,200,76]
-const spojenNiz = imena.concat(age)
-console.log(spojenNiz)
+// //spajanje dva niza  -contact (niz)
+// const imena = ["faris","isko", "aldin"]
+// const age = [20,100,50,120,200,76]
+// const spojenNiz = imena.concat(age)
+// console.log(spojenNiz)
 
-//trazenje clanas niza - find ()  // 
-// el prestavlja svaki clan svog niza
-const trazenaVrednost = cars.find(el => el > 50)
+// //trazenje clanas niza - find ()  // 
+// // el prestavlja svaki clan svog niza
+// const trazenaVrednost = cars.find(el => el > 50)
 
-//trazajne indeksa - finIndex
-const trazeniIndex = age.findIndex(el => el > 100) 
-
-
+// //trazajne indeksa - finIndex
+// const trazeniIndex = age.findIndex(el => el > 100) 
 
 
-console.log(trazeniIndex)
 
-console.log(trazenaVrednost)
+
+// console.log(trazeniIndex)
+
+// console.log(trazenaVrednost)
+
+// -----------------------------------HOME--------------------------------------------------
+
+
+// const imena = ["denis","faris","daris", "mesa", "adnan"]
+// imena.push("sara")
+// const sara = imena.pop();
+// console.log(sara)
+// console.log(imena)
+
+const godine = [12,76,26,6, 23,21,29,16]
+
+function punoletna_deca (ages){
+    let punoletni = [];
+    for(let i of ages){
+        if(i >=18){
+            punoletni.push(i)
+        };
+
+    }
+    return punoletni;
+    
+
+}
+
+
+console.log(punoletna_deca(godine));
+
+
 
 
 
