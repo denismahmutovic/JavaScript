1621e65cf5fd2a5fb9e8ec9198c84663b699e751
Denismahmutovic <djdeny95@gmail.com>
2022-06-09 22:18:49 +0200

    cas12

--- cas 12/app.js
+++ cas 12/app.js
@@ -35,25 +35,79 @@
 // }
 // console.log("radnik",radnik.first_name,radnik.last_name,radnik.age,radnik.city,radnik.radni_sati,radnik.radni_sati*radnik.dnevnica)
 
- const radnik = {
-    first_name: "Bob",           //this je Radnik
-     last_name : "Euroblok",
-     fullname:function(){
-        //  console.log(this)
-         return this.first_name +" " +  this.last_name;
-     },
-     adresa:"avnoja",
-     broj: "bb",
-     grad: "Novi Pazar",
-     getAdress:function (){
-         console.log(this)
-     }
+//  const radnik = {
+//     first_name: "Bob",           //this je Radnik
+//      last_name : "Euroblok",
+//      fullname:function(){
+//         //  console.log(this)
+//          return this.first_name +" " +  this.last_name;
+//      },
+//      adresa:{
+//          ulica:"avnoja",
+//          broj:"bb",
+//          grad:"Novi Pazar",
+//          getAdress:function(){
+//              return this.broj + " " + this.grad
+//          }
+//      }
     
-    }
-    
-    radnik.adresa.getAdress()
+//     };
+//     console.log(radnik.adresa.getAdress())
+//     console.log(radnik.fullname())
     
 //     function staJeOvdeThi(){
 //         console.log(this)
 //     }
 // staJeOvdeThi()
+
+// ----------------------------------------------------------------
+
+// const automobili = {
+//     marka: "Audi",
+//     model: "q7",
+//     boja: "Bela",
+//     pogon: "prednji",
+//     menjac: "Automatik",
+//     km:240000,
+//     contact:[33322,0633333],
+//     placanje:function(){
+//        return this.marka + " a boje je " + this.boja + " contact ta telefon " + this.contact 
+//     },
+//     graza:{
+//         parking:"50 Sat vremena",
+//         vikend:"od 17 free",
+//         placanje:" 7555",
+//         rampa: function(){
+//             return this.parking + " a viked cena je " + this.vikend + " posalji por na broj" + this.placanje
+//         }
+        
+//     }
+// }
+
+    
+//     // console.log("moj auto je marke", automobili["marka"], "i model je ", automobili.model,"presao", automobili["km"],"za vise infromacija pozovite na telefon",automobili["contact"])
+//    console.log(automobili.placanje())
+//    console.log(automobili.graza.rampa())
+ 
+   
+
+   
+ const name = {
+     firstname: "Denis",
+     lastname: " Mahmutovic",
+     FullName:function (homee){
+         console.log(this.firstname + this.lastname + " from "  + homee)
+     }
+ }
+ name.FullName("Novi Pazar")
+ 
+ const name2 = {
+     firstname: "Beni",
+     lastname: " Mahmutovic",
+     
+
+ }
+ name.FullName.call(name2, "Novi Sad")
+
+    
+
