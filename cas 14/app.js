008f87c8ae8b4f144fdbcd1f00287318a74fa222
Denismahmutovic <djdeny95@gmail.com>
2022-06-15 18:45:58 +0200

    cas15

--- cas 14/app.js
+++ cas 14/app.js
@@ -1,125 +1,131 @@
 
-// const students = [
-//     {name:"faris",grade: 8,year:2},
-//     {name:"isko",grade: 10,year:4},
-//     {name:"aldin",grade: 8,year:2},
-//     {name:"amina",grade: 9,year:4},
-//     {name:"sabina",grade: 9,year:3},
-//     {name:"denis",grade: 7,year:4},
-//     {name:"senad",grade: 6,year:1},
+// // const students = [
+// //     {name:"faris",grade: 8,year:2},
+// //     {name:"isko",grade: 10,year:4},
+// //     {name:"aldin",grade: 8,year:2},
+// //     {name:"amina",grade: 9,year:4},
+// //     {name:"sabina",grade: 9,year:3},
+// //     {name:"denis",grade: 7,year:4},
+// //     {name:"senad",grade: 6,year:1},
 
-// ];
-// function getAvgGrade(array){
-//     const ocene = array.map((el)=> el.grade )
-//     console.log(ocene)
-//     const prosek = ocene.reduce((total,value)=> total+value) / ocene.length;
-//     console.log(+prosek.toFixed(2))
-//     return +prosek.toFixed(2);
-
-
-// }
-// getAvgGrade(students);
+// // ];
+// // function getAvgGrade(array){
+// //     const ocene = array.map((el)=> el.grade )
+// //     console.log(ocene)
+// //     const prosek = ocene.reduce((total,value)=> total+value) / ocene.length;
+// //     console.log(+prosek.toFixed(2))
+// //     return +prosek.toFixed(2);
 
 
+// // }
+// // getAvgGrade(students);
 
 
 
-// niz koi sadrzi samo ocene
 
 
-// const person = {
-//     name:"john doe",
-//     age:22
-// };
-// function sayHello(){
-// console.log(`hello ${this.name}`)
-// }
-// sayHello.call(person)
-
-// ---------------------------HOME----------------------------------
+// // niz koi sadrzi samo ocene
 
 
-// const students = [
-//     {name:"faris",grade: 8,year:2},
-//     {name:"isko",grade: 10,year:4},
-//     {name:"aldin",grade: 8,year:2},
-//     {name:"amina",grade: 9,year:4},
-//     {name:"sabina",grade: 9,year:3},
-//     {name:"denis",grade: 7,year:4},
-//     {name:"senad",grade: 6,year:1},
-// ]
-// function getAvgGrade(array){
-//     const ocene = array.map((el)=> el.grade);
-//     const zbir = ocene.reduce((total,value)=> total+value ) / ocene.length;
-//     console.log(+zbir.toFixed(2))
-// }
-// getAvgGrade(students)
+// // const person = {
+// //     name:"john doe",
+// //     age:22
+// // };
+// // function sayHello(){
+// // console.log(`hello ${this.name}`)
+// // }
+// // sayHello.call(person)
 
-// const person = {
-//     name:"John Doe",
-//     age:22,
+// // ---------------------------HOME----------------------------------
+
+
+// // const students = [
+// //     {name:"faris",grade: 8,year:2},
+// //     {name:"isko",grade: 10,year:4},
+// //     {name:"aldin",grade: 8,year:2},
+// //     {name:"amina",grade: 9,year:4},
+// //     {name:"sabina",grade: 9,year:3},
+// //     {name:"denis",grade: 7,year:4},
+// //     {name:"senad",grade: 6,year:1},
+// // ]
+// // function getAvgGrade(array){
+// //     const ocene = array.map((el)=> el.grade);
+// //     const zbir = ocene.reduce((total,value)=> total+value ) / ocene.length;
+// //     console.log(+zbir.toFixed(2))
+// // }
+// // getAvgGrade(students)
+
+// // const person = {
+// //     name:"John Doe",
+// //     age:22,
     
-// };
-// function sayHello(){
-//     console.log(`Hello ${this.name}`)
-// }
-// sayHello.call(person)
+// // };
+// // function sayHello(){
+// //     console.log(`Hello ${this.name}`)
+// // }
+// // sayHello.call(person)
 
 
 
-// const JaSam = {
+// // const JaSam = {
+// //     ime:"Denis",
+// //     Prezime:'mahmutovic',
+// //     godine:26,
+// //     Posao:"DEEJAY"
+// // }
+// // function JaSamm(){
+// //     console.log(`ja sam ${ime}`)
+// // }
+// // JaSamm.call(JaSam)
+// // let hrana = {
+// //     Rostilj:"Hambuger,Belo meso, Batak, Virsle",
+// //     BrzanaHrana:"Topli,Pizza,Piroska"
+
+// // }
+// // function Kasa(text,ocena){
+// //     return `${this.BrzanaHrana} ${text} ${ocena}`
+// // }
+// // console.log(Kasa.call(hrana,"je ukusna ", 10))
+// // console.log(Kasa.apply(hrana,["Nije ukusan", 6]))
+
+// // const bindFunc = Kasa.bind(hrana)
+// // console.log(bindFunc("nije nesto",9))
+
+// // const NumArray = [10,30,40,5,100,200,1000]
+// // console.log(Math.max.apply(null,NumArray))
+
+// // const ime = "Denis M"
+// // const zanimanje = "Deejay"
+// // const godine = 26
+// // console.log(`ja sam ${ime} i Moje zanimanje je ${zanimanje} i imam ${godine} Godina`)
+// const zanimanje = {
 //     ime:"Denis",
 //     Prezime:'mahmutovic',
 //     godine:26,
-//     Posao:"DEEJAY"
+//     Posao:"DEEJAY",
 // }
-// function JaSamm(){
-//     console.log(`ja sam ${ime}`)
+// function dj(){
+//     console.log(`ja sam ${this.ime} i ja sam ${this.Posao}`)
 // }
-// JaSamm.call(JaSam)
-// let hrana = {
-//     Rostilj:"Hambuger,Belo meso, Batak, Virsle",
-//     BrzanaHrana:"Topli,Pizza,Piroska"
+// dj.call(zanimanje)
 
-// }
-// function Kasa(text,ocena){
-//     return `${this.BrzanaHrana} ${text} ${ocena}`
-// }
-// console.log(Kasa.call(hrana,"je ukusna ", 10))
-// console.log(Kasa.apply(hrana,["Nije ukusan", 6]))
-
-// const bindFunc = Kasa.bind(hrana)
-// console.log(bindFunc("nije nesto",9))
-
-// const NumArray = [10,30,40,5,100,200,1000]
-// console.log(Math.max.apply(null,NumArray))
-
-// const ime = "Denis M"
-// const zanimanje = "Deejay"
-// const godine = 26
-// console.log(`ja sam ${ime} i Moje zanimanje je ${zanimanje} i imam ${godine} Godina`)
-const zanimanje = {
-    ime:"Denis",
-    Prezime:'mahmutovic',
-    godine:26,
-    Posao:"DEEJAY",
-}
-function dj(){
-    console.log(`ja sam ${this.ime} i ja sam ${this.Posao}`)
-}
-dj.call(zanimanje)
-
-const hrana = {
-    food: "Pizza, Piroksa",
-    Rostilj:"Hambuger, Belo meso"
+// const hrana = {
+//     food: "Pizza, Piroksa",
+//     Rostilj:"Hambuger, Belo meso"
     
-}
-function Kasa (text,ocena){
-    return `${this.Rostilj} ${text} ${ocena}`
+// }
+// function Kasa (text,ocena){
+//     return `${this.Rostilj} ${text} ${ocena}`
     
-}
-function Kasaa(text,ocena){
-    return `${this.food} ${text} ${ocena}`
-}
-console.log(Kasaa.call(hrana,"je predivna ocena", 10))
-console.log(Kasa.apply(hrana,["je pdevivna ocena",9]))
+// }
+// function Kasaa(text,ocena){
+//     return `${this.food} ${text} ${ocena}`
+// }
+// console.log(Kasaa.call(hrana,"je predivna ocena", 10))
+// console.log(Kasa.apply(hrana,["je pdevivna ocena",9]))
+
+// const DATA = [10,20,30,100,2000]
+// console.log(Math.round.apply(null,DATA))
+
+
+
---
+++ cas 15/app.js
@@ -0,0 +1,219 @@
+// const watchList = [
+//     {
+//       "Title": "Inception",
+//       "Year": "2010",
+//       "Rated": "PG-13",
+//       "Released": "16 Jul 2010",
+//       "Runtime": "148 min",
+//       "Genre": "Action, Adventure, Crime",
+//       "Director": "Christopher Nolan",
+//       "Writer": "Christopher Nolan",
+//       "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+//       "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//       "Language": "English, Japanese, French",
+//       "Country": "USA, UK",
+//       "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//       "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//       "Metascore": "74",
+//       "imdbRating": "8.8",
+//       "imdbVotes": "1,446,708",
+//       "imdbID": "tt1375666",
+//       "Type": "movie",
+//       "Response": "True"
+//     },
+//     {
+//       "Title": "Interstellar",
+//       "Year": "2014",
+//       "Rated": "PG-13",
+//       "Released": "07 Nov 2014",
+//       "Runtime": "169 min",
+//       "Genre": "Adventure, Drama, Sci-Fi",
+//       "Director": "Christopher Nolan",
+//       "Writer": "Jonathan Nolan, Christopher Nolan",
+//       "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//       "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//       "Language": "English",
+//       "Country": "USA, UK",
+//       "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//       "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//       "Metascore": "74",
+//       "imdbRating": "8.6",
+//       "imdbVotes": "910,366",
+//       "imdbID": "tt0816692",
+//       "Type": "movie",
+//       "Response": "True"
+//     },
+//     {
+//       "Title": "The Dark Knight",
+//       "Year": "2008",
+//       "Rated": "PG-13",
+//       "Released": "18 Jul 2008",
+//       "Runtime": "152 min",
+//       "Genre": "Action, Adventure, Crime",
+//       "Director": "Christopher Nolan",
+//       "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//       "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//       "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//       "Language": "English, Mandarin",
+//       "Country": "USA, UK",
+//       "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//       "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//       "Metascore": "82",
+//       "imdbRating": "9.0",
+//       "imdbVotes": "1,652,832",
+//       "imdbID": "tt0468569",
+//       "Type": "movie",
+//       "Response": "True"
+//     },
+//     {
+//       "Title": "Batman Begins",
+//       "Year": "2005",
+//       "Rated": "PG-13",
+//       "Released": "15 Jun 2005",
+//       "Runtime": "140 min",
+//       "Genre": "Action, Adventure",
+//       "Director": "Christopher Nolan",
+//       "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//       "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//       "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//       "Language": "English, Urdu, Mandarin",
+//       "Country": "USA, UK",
+//       "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//       "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//       "Metascore": "70",
+//       "imdbRating": "8.3",
+//       "imdbVotes": "972,584",
+//       "imdbID": "tt0372784",
+//       "Type": "movie",
+//       "Response": "True"
+//     },
+//     {
+//       "Title": "Avatar",
+//       "Year": "2009",
+//       "Rated": "PG-13",
+//       "Released": "18 Dec 2009",
+//       "Runtime": "162 min",
+//       "Genre": "Action, Adventure, Fantasy",
+//       "Director": "James Cameron",
+//       "Writer": "James Cameron",
+//       "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//       "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//       "Language": "English, Spanish",
+//       "Country": "USA, UK",
+//       "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//       "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//       "Metascore": "83",
+//       "imdbRating": "7.9",
+//       "imdbVotes": "876,575",
+//       "imdbID": "tt0499549",
+//       "Type": "movie",
+//       "Response": "True"
+//     }
+//   ];
+  
+//   function getRating(watchList) {
+//     // Only change code below this line
+//     const trazi = watchList.filter(movie=> movie.Director === "chirstopher Nolan")  
+//     console.log(trazi)
+    
+//     // Only change code above this line
+//     return averageRating;
+//   }
+  
+//   console.log(getRating(watchList));
+
+
+
+
+// const squareList = arr => {
+//     const positivenNums = arr.filter(num => num > 0 && num % parseInt(num)== 0)
+//     const zbir = positivenNums.map(num=> num*num)
+//     return zbir
+//     //uzima samo broj prvi broj ne vraca posle zareza
+//     console.log(positivenNums)
+//     // Only change code below this line
+//     return arr;
+//     // Only change code above this line
+//   };
+  
+//   const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+//   console.log(squaredIntegers);
+
+
+//   const person1 = {
+//       name: "john doe",
+//       age:48,
+//       job:"Emloyed",
+//       adress:"Usa",
+//       kids:4,
+//   }
+//   const person2 = {
+//     name: "john doe",
+//     age:48,
+//     job:"Emloyed",
+//     adress:"Usa",
+//     kids:4,
+// };
+//     const person2 = person1
+//      person2.name = "no name"
+//   console.log(Object.is(person1,person2))
+  
+  
+    //nizu isti jer nisu za istu memoristkiu lokaciju
+
+//   Object.seal(person)
+//   person.name = "denis"
+//   person.age =  49   // object.seal moze da menja vrednost zima koi je vec formiran
+  
+//   console.log(Object.keys(person))  // uzizamo samo kluceve name age job..
+//   console.log(Object.values(person)) //
+//   Object.freeze(person) //zamzuzni objekat
+
+//   person.phone = "1231212312"  ///dodavanje metoda u niz 
+//   console.log(Object.entries(person)) // vraca niz koji su dva clana
+  
+    // spret orerator ---- 
+
+// const names = ["faris","aldin","senad","dzenan","sabina"]  
+// const newNames = ["isko",...names,"hamza"]   // ubacivanje 
+// console.log(names)
+// console.log(newNames)
+
+
+// const numbers = [1,2,3,1,5,2,7,10]
+// const names = ["john","cena","denis"]
+// const spoji = [...names,...numbers]  // dodajemo iz jednom u drugin u mesto concat
+// console.log(spoji) 
+// const nemaDupli = [...new Set (numbers)]  //niz   .../spret operatori
+// console.log(nemaDupli)    /// da se brojevi ne dupliraju sa new Set
+
+
+const person = {
+    name: "denis",
+    state:"novi pazar"
+};
+const person2 = {
+    ...person,
+    phone: 323232,
+    aderasa:"Beograd, Novi sad"
+}
+// console.log(person);
+console.log(person2);
+  
+ 
+const movies = [
+    {title: "movie1",rating:8,budget:"230m"},
+    {title: "movie2",rating:5,budget:"230m"},
+    {title: "movie3",rating:9,budget:"230m"},
+    {title: "movie4",rating:4,budget:"230m"},
+    {title: "movie5",rating:9.5,budget:"230m"},
+
+]
+const newMoviesArray = movies.map((movie)=> {
+    return{
+        title:movie.title,
+        budget:movie.budget
+    }
+})
+console.log(newMoviesArray)
+  
