bd2aad28a1159e5abc61fc7aedc0a0a4eb49e453
Denismahmutovic <djdeny95@gmail.com>
2022-06-20 21:53:46 +0200

    promise

--- cas 17/app.js
+++ cas 17/app.js
@@ -10,3 +10,36 @@
 // .finally/
 
 
+
+
+
+posao = new Promise((resolve,reject)=>{
+    response = 200;
+    for(let i = 0;i < 9000000;i++){}
+        if(response === 200){
+            resolve([1,2,3,4,5]);  
+        } else{
+            reject("GREEESKAA")
+        }
+        
+    }  
+)
+posao.then((data)=>{
+    console.log("uspesno povukao podatke",data);
+    arr = data.filter((el)=> el % 2 === 0)
+    
+    return arr; 
+
+})
+.then((arr)=> {
+    console.log("druga func",arr)  //parni brojevi
+})
+.catch((err)=>{
+    console.log("neuspesno povukao podatke",err)
+   
+    
+})
+// ----------------------------------------------------------------.
+
+
+
