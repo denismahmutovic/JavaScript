e3d79b9cdc8caa68dc1fe2b9c2603b7e7b16543f
Denismahmutovic <djdeny95@gmail.com>
2022-07-14 22:57:03 +0200

    vezba

--- JavaScript-Dom/app.js
+++ JavaScript-Dom/app.js
@@ -1,50 +1,88 @@
-const bredn = document.getElementsByTagName("span");
-for(auto of bredn){
-    console.log(auto.innerText)
-}
-// console.log(bredn[0].innerText)
+// const bredn = document.getElementsByTagName("span");
+// for(auto of bredn){
+//     console.log(auto.innerText)
+// }
+// // console.log(bredn[0].innerText)
 
-let model = document.getElementsByClassName("automobili-model");
+// let model = document.getElementsByClassName("automobili-model");
+// // for(modeli of model){
+// //     console.log(modeli.innerText)
+// // }
+// // let najbolji = document.getElementById("recenica")
+// // console.log(najbolji.innerText)
+
+
+
+// brend = document.querySelectorAll("span")
+// console.log(brend)
+// for(auto of brend){
+//     console.log(auto.innerText)
+// }
+
+// let modeli = document.querySelector(".automobili-model")
 // for(modeli of model){
 //     console.log(modeli.innerText)
 // }
-// let najbolji = document.getElementById("recenica")
-// console.log(najbolji.innerText)
+
+// // Brisanje iz HTML remove()
+
+// let recenica = document.querySelector("#recenica");
+// recenica.remove()
+
+// // Kreiranje novog Div elementa
+
+// // let novi_el = document.createElement("div")
+// // let body = document.querySelector("body")
+// // body.appendChild(novi_el)
+// // novi_el.classList = "novi-element";
+// // novi_el.innerText = "pozdrav!"
 
 
-
-brend = document.querySelectorAll("span")
-console.log(brend)
-for(auto of brend){
-    console.log(auto.innerText)
-}
-
-let modeli = document.querySelector(".automobili-model")
-for(modeli of model){
-    console.log(modeli.innerText)
-}
-
-// Brisanje iz HTML remove()
-
-let recenica = document.querySelector("#recenica");
-recenica.remove()
-
-// Kreiranje novog Div elementa
-
-// let novi_el = document.createElement("div")
+// let novi = document.createElement("div")
 // let body = document.querySelector("body")
-// body.appendChild(novi_el)
-// novi_el.classList = "novi-element";
-// novi_el.innerText = "pozdrav!"
+// body.appendChild(novi) // na kraju body taga dodajmo nas novi element
+// novi.classList = "denis"
+// novi.innerText = "kralj"
+
+// function pokreniMe(dugme){
+//     let nazivDugmeta = dugme.innerText;
+//     let klasaDugmeta = dugme.className
+    
+    
+    
+//     let message = document.querySelector("#kliknutoDugme")
+//     message.innerText = nazivDugmeta + "- " + klasaDugmeta
+    
+// }
 
 
-let novi = document.createElement("div")
-let body = document.querySelector("body")
-body.appendChild(novi) // na kraju body taga dodajmo nas novi element
-novi.classList = "denis"
-novi.innerText = "kralj"
+// function testing(){
+//     let link2 = document.querySelector(".drugi-link")
+//     // console.log(link2.previousElementSibling) // poziva prehodni elemenat
+//     // console.log(link2.nextElementSibling); // poziva sledeci elemenet
+//     // link2.setAttribute("class","denis") // promena classa na click
+//     link2.removeAttribute("data-test") // brisanje atributa data,clasa ...
+
+// }
 
 
+                //ubacinje elementa u div preko innerHtlm
+                
 
+// function testing(){
+//     // let gl_div= document.querySelector(".glavni-div")
+//     // gl_div.innerHTML = `<span> Klikni ovaj link: <a href="#">klick</a></span>` 
+
+// }
+function validacija(){
+    let input = document.querySelector("#email")
+    let value = input.value
+    if(value.includes("@") && value.includes(".")){
+        
+    } else{
+        console.log("Mail nije validan")
+    }
+    
+}
 
 
