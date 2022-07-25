831765b5fcb9a51498ff7180f7f51f3dfd6c98f1
Denismahmutovic <djdeny95@gmail.com>
2022-07-25 18:28:25 +0200

    domPrvi-cas

---
+++ JAVA.DOM/java.js
@@ -0,0 +1,23 @@
+const glavniDiv = document.querySelector("#pocetak")
+const body = document.querySelector("body")
+const paragraf = document.createElement("p")
+paragraf.textContent = "ovo je p dodat i js"
+glavniDiv.append(paragraf)
+glavniDiv.classList.add("kocka") // dodavanje classe
+paragraf.classList.add("text")
+const dugme = document.createElement("button")
+dugme.innerText = "Click me"
+glavniDiv.append(dugme)
+const onButtonClick = ()=>{
+    paragraf.classList.toggle("veci-fond")
+    skriveniDiv.classList.toggle("nevidljiv")
+
+}
+dugme.addEventListener("click",)
+
+paragraf.classList.toggle("veci-fond",onButtonClick)
+
+const skriveniDiv = document.createElement("div")
+skriveniDiv.innerHTML = `<h1> ovo je H1 </>
+                        <p> neki paragrag</p>`
+
